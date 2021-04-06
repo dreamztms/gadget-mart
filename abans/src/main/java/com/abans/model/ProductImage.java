326a13b8be2156af1d559d3694609e2a285201f5
Thilina Madusanka <thilinamullewidane@gmail.com>
2021-04-06 10:47:07 +0530

    updated abans microservice

--- abans/src/main/java/com/abans/model/ProductImage.java
+++ abans/src/main/java/com/abans/model/ProductImage.java
@@ -1,7 +1,13 @@
 package com.abans.model;
 
+import lombok.Data;
+
 /**
  * @author thilinamullewidane
  */
+@Data
 public class ProductImage {
+    private int imageId;
+    private String productId;
+    private String imageUrl;
 }
--- abans/src/main/java/com/abans/repository/impl/ApiRepositoryImpl.java
+++ abans/src/main/java/com/abans/repository/impl/ApiRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.abans.repository.impl;
 
 import com.abans.model.Product;
 import com.abans.model.ProductFilter;
+import com.abans.model.ProductImage;
 import com.abans.repository.ApiRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,10 +31,33 @@ public class ApiRepositoryImpl implements ApiRepository {
                 Collections.emptyMap(),
                 (rs, i) -> {
                     Product product = new Product();
-                    product.setProductId(rs.getInt("PRODUCT_ID"));
+                    product.setProductId(rs.getString("productId"));
+                    product.setProductName(rs.getString("productName"));
+                    product.setProductDescription(rs.getString("productDescription"));
+                    product.setPrice(rs.getInt("price"));
+                    product.setQty(rs.getInt("qty"));
+                    product.setType(rs.getString("type"));
+                    product.setProductImages(getProductList(rs.getString("productId")));
+
 
 
                     return product;
                 });
     }
+
+    private List<ProductImage> getProductList(String productId) {
+        StringBuilder getContactsQuery = new StringBuilder("select * from product_images where product_id = :productId");
+
+        return namedParameterJdbcTemplate.query(
+                getContactsQuery.toString(),
+                Collections.singletonMap("productId",productId),
+                (rs, i) -> {
+                    ProductImage productImage = new ProductImage();
+                    productImage.setProductId(rs.getString("PRODUCT_ID"));
+                    productImage.setImageId(rs.getInt("IMAGE_ID"));
+                    productImage.setImageUrl(rs.getString("IMAGE_URL"));
+                    return productImage;
+                });
+    }
+
 }
